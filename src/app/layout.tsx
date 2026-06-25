4173c8bd69325f8e4d69395c5807206d34e85ddd
geisajanuariouba-cloud <sliviaaz.uba@gmail.com>
2026-06-25 17:47:20 -0300

    feat: add Utmify UTM capture script to forward ad params to checkout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -70,6 +70,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','1274073178133799');fbq('track','PageView');`,
           }}
         />
+        {/* Utmify — captura UTMs do anúncio e repassa para o checkout da Kiwify */}
+        <Script
+          id="utmify-utms"
+          src="https://cdn.utmify.com.br/scripts/utms/latest.js"
+          strategy="afterInteractive"
+          data-utmify-prevent-xcod-sck
+          data-utmify-prevent-subids
+        />
       </body>
     </html>
   );
